0a2933d59b6313c4cd2cd3160b8d566a4df54d13
Ankitkj1999 <57802410+Ankitkj1999@users.noreply.github.com>
2020-06-03 20:18:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_implicit_animation
+ 
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiscData {
+  static final _rng = Random();
+
+  double size;
+  Color color;
+  Alignment alignment;
+
+  DiscData() {
+    color = Color.fromARGB(
+      _rng.nextInt(200),
+      _rng.nextInt(255),
+      _rng.nextInt(255),
+      _rng.nextInt(255),
+    );
+    size = _rng.nextDouble() * 40 + 10;
+    alignment = Alignment(
+      _rng.nextDouble() * 2 - 1,
+      _rng.nextDouble() * 2 - 1,
+    );
+  }
+}
+
+void main() async {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          color: Color(0xFF15202D),
+          child: SizedBox.expand(
+            child: VariousDiscs(50),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+class VariousDiscs extends StatefulWidget {
+  final numberOfDiscs;
+
+  VariousDiscs(this.numberOfDiscs);
+  
+  @override
+  _VariousDiscsState createState() => _VariousDiscsState();
+}
+
+class _VariousDiscsState extends State<VariousDiscs> {
+  final _discs = <DiscData>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _makeDiscs();
+  }
+
+  void _makeDiscs() {
+    _discs.clear();
+    for (int i = 0; i < widget.numberOfDiscs; i++) {
+      _discs.add(DiscData());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => setState(() {
+        _makeDiscs();
+      }),
+      child: Stack(
+        children: [
+          Center(
+            child: Text(
+              'Click a disc!',
+              style: TextStyle(color: Colors.white, fontSize: 50),
+            ),
+          ),
+          for (final disc in _discs)
+            Positioned.fill(
+              child: AnimatedAlign(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.easeInOut,
+                alignment: disc.alignment,
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  decoration: BoxDecoration(
+                    color: disc.color,
+                    shape: BoxShape.circle,
+                  ),
+                  height: disc.size,
+                  width: disc.size,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
